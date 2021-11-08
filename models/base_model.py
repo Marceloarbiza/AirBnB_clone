947ba42eee651dcfb057b6e7c14986e9f1ab9cfc
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-08 13:20:45 -0800

    try to complete task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
+from models.__init__ import storage
 import uuid
 from datetime import datetime
 
 class BaseModel:
     """ Write a class BaseModel that defines all common attributes/methods for other classes """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and len(kwargs) >= 1:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """ print [<class name>] (<self.id>) <self.__dict__> """
@@ -17,6 +26,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
         self.updated_at = datetime.today()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
@@ -25,4 +35,3 @@ class BaseModel:
         dicRet['created_at'] = str(self.created_at.isoformat())
         dicRet['updated_at'] = str(self.updated_at.isoformat())
         return dicRet
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python3
 
+from models.base_model import BaseModel
+import json
+
+listclass = {'BaseModel': BaseModel}
+"""
+listclass = {"BaseModel": BaseModel, "User": User, "State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+"""
 class FileStorage:
     def __init__(self):
-        __file_path = 
+        self.__file_path = './file.json'
+        self.__objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        name = str(obj.__class__.__name__)
+        idName = str(obj.id)
+        name = name + '.' + idName
+        self.__objects[name] = str(obj.to_dict())
+
+    def save(self):
+        """  serializes __objects to the JSON file (path: __file_path) """
+        with open(self.__file_path, 'w') as file:
+            dicto = {x.to_dict for x in self.__objects}
+            file.write(json.dumps(dicto))
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists;
+        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        try:
+            with open( self.__file_path, 'r') as f:
+                obj = json.loads(f.read())
+            for i in obj:
+                self.__objects[i] = listclass[obj[i]["__class__"]](**obj[i])
+        except:
+            pass
+
