e89de1ad4d59e6a974d295f3060f8dbbbb68cfad
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-10 07:37:30 -0800

    try to make 8

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+#            models.storage.new(self)
 
     def __str__(self):
         """ print [<class name>] (<self.id>) <self.__dict__> """
@@ -26,9 +27,9 @@ class BaseModel:
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
-        from models.__init__ import storage
+        from models.base_model import BaseModel
         self.updated_at = datetime.today()
-        models.storage.new(self)
+#        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
