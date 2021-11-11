b19ad7f31dee68856046d636d9611649a760ff5f
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-11 06:16:55 -0800

    working in task 8 "update"

--- console.py
+++ console.py
@@ -34,7 +34,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_show(self, arg):
-        """Prints the string representation of an instance based on the class name and id"""
+        """Prints the string representation of an instance based on the class name and id\n"""
         listArg = arg.split(' ')
         if listArg[0] == '':
             print('** class name missing **')
@@ -51,7 +51,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id\n"""
         listArg = arg.split(' ')
         if listArg[0] == '':
             print('** class name missing **')
@@ -64,7 +64,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id (save the change into the JSON file)"""
+        """Deletes an instance based on the class name and id (save the change into the JSON file)\n"""
         listArg = arg.split(' ')
         if listArg[0] == '':
             print('** class name missing **')
@@ -76,31 +76,43 @@ class HBNBCommand(cmd.Cmd):
             compare = (listArg[0] + '.' + listArg[1])
             if compare in models.storage.all():
                 del (models.storage.all()[compare])
+                #FALTA GUARDARLO?
             else:
                 print('** no instance found **')
         pass
 
-        pass
-
     def do_update(self, arg):
         """Updates an instance based on the class name and id by adding
-        or updating attribute (save the change into the JSON file)."""
+        or updating attribute (save the change into the JSON file).\n"""
+        listArg = arg.split(' ')
+        if listArg[0] == '':
+            print('** class name missing **')
+        elif listArg[0] not in listclass:
+            print("** class doesn't exist **")
+        elif (listArg[0] in listclass) and len(listArg) == 1:
+            print('** instance id missing **')
+        elif len(listArg) == 2:
+            compare = (listArg[0] + '.' + listArg[1])
+            if (compare not in models.storage.all()):
+                print('** no instance found **')
+            elif (compare in models.storage.all()):
+                if listArg[1] not in dir(models.storage.all()):
+                    print('** attribute name missing **')
         pass
 
     def do_all(self, arg):
-        """ Prints all string representation of all instances based or not on the class name """
+        """ Prints all string representation of all instances based or not on the class name\n"""
         listArg = arg.split(' ')
         if (arg == ''):
             for key in models.storage.all():
-                print(models.storage.all()[key])
-
+                print(str(models.storage.all()[key]))
         else:
             if (listArg[0] not in listclass):
                 print("** class doesn't exist **")
             elif len(listArg) == 1:
-                for key in models.storage.all().values():
-                    if key.__class__.__name__ == listArg[0]:
-                        print(key)
+                for value in models.storage.all().values():
+                    if value.__class__.__name__ == listArg[0]:
+                        print(str(value))
         pass
 
 if __name__ == '__main__':
